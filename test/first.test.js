bad4fd0142eeb2f8de7e4d94302029519a65e8e9
mashihara <mashiharahiroyuki@gmail.com>
2018-05-09 08:10:11 +0900

    fix: first.test, add: last.test

--- test/first.test.js
+++ test/first.test.js
@@ -1,19 +1,52 @@
 const _ = require('../underbar');
 
 describe('first()', () => {
-  it('returns the first element of an array', () => {
-    expect(_.first(['a', 'b', 'c'])).toEqual('a');
-  });
+  describe('arg1 is multiple element', () =>{
 
-  it('returns the first 2 elements of an array', () => {
-    expect(_.first(['a', 'b', 'c'], 2)).toEqual(['a', 'b']);
-  });
+    it('returns the first element of an array', () => {
+      expect(_.first(['a', 'b', 'c'])).toEqual('a');
+    });
 
-  it('returns the whole array if you ask for more elements than it has', () => {
-    expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
-  });
+    it('returns the first 2 elements of an array', () => {
+      expect(_.first(['a', 'b', 'c'], 2)).toEqual(['a', 'b']);
+    });
 
-  it('returns the first element of an array that have only one element', () => {
-    expect(_.first(['a'])).toEqual('a');
-  });
-});
+    it('returns the whole array if you ask for more elements than it has', () => {
+      expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
+    });
+
+    it('returns the first elements of an array when arg2 is 1', () => {
+      expect(_.first(['a', 'b', 'c'], 1)).toEqual('a');
+    });
+    
+    it('returns the empty array', () => {
+      expect(_.first(['a', 'b', 'c'], 0)).toEqual([]);
+    });
+
+    it('returns the first 2 elements of an array when arg2 is minus number', () => {
+      expect(_.first(['a', 'b', 'c'], -1)).toEqual(['a', 'b']);
+    });
+  })
+
+  describe('arg1 is one element', () =>{
+    it('arg2:minus number', () => {
+      expect(_.first(['a'],-1)).toEqual([]);
+    });
+
+    it('arg2: 0', () => {
+      expect(_.first(['a'],0)).toEqual([]);
+    });
+
+    it('arg2: none', () => {
+      expect(_.first(['a'])).toEqual('a');
+    });
+
+    it('arg2: 1', () => {
+      expect(_.first(['a'],1)).toEqual('a');
+    });
+
+    it('arg2: 2', () => {
+      expect(_.first(['a'],2)).toEqual(['a']);
+    });
+  })
+})
--- test/last.test.js
+++ test/last.test.js
@@ -1,15 +1,52 @@
 const _ = require('../underbar');
 
 describe('last()', () => {
-  it('returns the last element of an array', () => {
-    expect(_.last(['a', 'b', 'c'])).toEqual('c');
-  });
+  describe('arg1 is multiple element', () =>{
 
-  it('returns the last 2 elements of an array', () => {
-    expect(_.last(['a', 'b', 'c'], 2)).toEqual(['b', 'c']);
-  });
+    it('returns the last element of an array', () => {
+      expect(_.last(['a', 'b', 'c'])).toEqual('c');
+    });
 
-  it('returns the whole array if you ask for more elements than it has', () => {
-    expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
-  });
+    it('returns the last 2 elements of an array', () => {
+      expect(_.last(['a', 'b', 'c'], 2)).toEqual(['b', 'c']);
+    });
+
+    it('returns the whole array if you ask for more elements than it has', () => {
+      expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
+    });
+
+    it('returns the last 2 elements of an array when arg2 is 1', () => {
+      expect(_.last(['a', 'b', 'c'], 1)).toEqual('c');
+    });
+    
+    it('returns the empty array', () => {
+      expect(_.last(['a', 'b', 'c'], 0)).toEqual([]);
+    });
+
+    it('returns the empty array when arg2 is minus number', () => {
+      expect(_.last(['a', 'b', 'c'], -1)).toEqual([]);
+    });
+  })
+
+  describe('arg1 is one element', () =>{
+    it('arg2: minus number', () => {
+      expect(_.last(['a'],-1)).toEqual([]);
+    });
+  
+    it('arg2: 0', () => {
+      expect(_.last(['a'],0)).toEqual([]);
+    });
+  
+    it('arg2: none', () => {
+      expect(_.last(['a'])).toEqual('a');
+    });
+  
+    it('arg2: 1', () => {
+      expect(_.last(['a'],1)).toEqual('a');
+    });
+    
+    it('arg2: 2', () => {
+      expect(_.last(['a'],2)).toEqual(['a']);
+    });  
+  })
 });
